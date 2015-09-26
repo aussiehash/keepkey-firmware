492ca68a631dc776151e8a3549584bab947f7b52
aussiehash <mbbs@ymail.com>
2015-09-27 01:01:46 +1000

    MessageType_Peek, MessageType_PeekResponse, _Peek, _PeekResponse, Peek_address_tag, Peek_length_tag, PeekResponse_data_tag, Peek_fields, PeekResponse_fields, Peek_size, PeekResponse_size
    +    MessageType_MessageType_Peek = 200,
    +    MessageType_MessageType_PeekResponse = 201
    
    //////
    +typedef struct _Peek {
    +    uint32_t address;
    +    uint32_t length;
    +} Peek;
    +
    +typedef struct {
    +    size_t size;
    +    uint8_t bytes[1024];
    +} PeekResponse_data_t;
    +
    +typedef struct _PeekResponse {
    +    PeekResponse_data_t data;
    +} PeekResponse;
    +
    /////
    +#define Peek_address_tag                         1
    +#define Peek_length_tag                          2
    +#define PeekResponse_data_tag                    1
    //////
    +extern const pb_field_t Peek_fields[3];
    +extern const pb_field_t PeekResponse_fields[2];
    /////
    +#define Peek_size                                12
    +#define PeekResponse_size                        1027

--- interface/public/messages.pb.h
+++ interface/public/messages.pb.h
@@ -69,7 +69,9 @@ typedef enum _MessageType {
     MessageType_MessageType_DebugLinkState = 102,
     MessageType_MessageType_DebugLinkStop = 103,
     MessageType_MessageType_DebugLinkLog = 104,
-    MessageType_MessageType_DebugLinkFillConfig = 105
+    MessageType_MessageType_DebugLinkFillConfig = 105,
+    MessageType_MessageType_Peek = 200,
+    MessageType_MessageType_PeekResponse = 201
 } MessageType;
 
 /* Struct definitions */
@@ -491,6 +493,20 @@ typedef struct _PassphraseAck {
     char passphrase[51];
 } PassphraseAck;
 
+typedef struct _Peek {
+    uint32_t address;
+    uint32_t length;
+} Peek;
+
+typedef struct {
+    size_t size;
+    uint8_t bytes[1024];
+} PeekResponse_data_t;
+
+typedef struct _PeekResponse {
+    PeekResponse_data_t data;
+} PeekResponse;
+
 typedef struct _PinMatrixAck {
     char pin[10];
 } PinMatrixAck;
@@ -882,6 +898,9 @@ extern const char SimpleSignTx_coin_name_default[17];
 #define MessageSignature_address_tag             1
 #define MessageSignature_signature_tag           2
 #define PassphraseAck_passphrase_tag             1
+#define Peek_address_tag                         1
+#define Peek_length_tag                          2
+#define PeekResponse_data_tag                    1
 #define PinMatrixAck_pin_tag                     1
 #define PinMatrixRequest_type_tag                1
 #define Ping_message_tag                         1
@@ -988,6 +1007,8 @@ extern const pb_field_t DebugLinkState_fields[15];
 extern const pb_field_t DebugLinkStop_fields[1];
 extern const pb_field_t DebugLinkLog_fields[4];
 extern const pb_field_t DebugLinkFillConfig_fields[1];
+extern const pb_field_t Peek_fields[3];
+extern const pb_field_t PeekResponse_fields[2];
 
 /* Maximum encoded size of messages (where known) */
 #define Initialize_size                          0
@@ -1047,6 +1068,8 @@ extern const pb_field_t DebugLinkFillConfig_fields[1];
 #define DebugLinkStop_size                       0
 #define DebugLinkLog_size                        300
 #define DebugLinkFillConfig_size                 0
+#define Peek_size                                12
+#define PeekResponse_size                        1027
 
 #ifdef __cplusplus
 } /* extern "C" */
